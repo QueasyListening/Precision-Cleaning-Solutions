7a919ece39364d7803dffc0566d73c22547ea25b
joshuacoyne <josh.j.coyne@gmail.com>
2019-05-24 13:10:07 -0400

    added sparkle effect on logo animation

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 const triangles = Array.from(document.getElementsByClassName("triangle"));
 
-triangles.forEach((triangle, i) => {
-    if (i === 0) {
-        triangle.style.transform = 'rotate(0deg) scale(1)';
-    } else {
-        window.setTimeout(() => {
-            triangle.style.transform = `rotate(${i*60}deg) scale(1)`;
-        }, 200*i);
-    }
-});
+window.addEventListener('load', () => {
+    triangles.forEach((triangle, i) => {
+        if (i === 0) {
+            triangle.style.transform = 'scale(1)';
+            // triangle.style.transform = 'rotate(0deg)';
+        } else {
+            window.setTimeout(() => {
+                triangle.style.transform = `rotate(${i*60}deg)`;
+            }, 300*i);
+        }
+    });
+    
+    window.setTimeout(() => {
+        const p = document.getElementsByClassName('p')[0];
+        p.style.transform="scale(1) rotate(-30deg)";
+        p.style.opacity="1";
+    }, 1250);
 
-window.setTimeout(() => {
-    const p = document.getElementsByClassName('p')[0];
-    p.style.transform="scale(1) rotate(-30deg)";
-    p.style.opacity="1";
-}, 650);
+    window.setTimeout(() => {
+        console.log(document.getElementById('twinkle'));
+        document.getElementById('twinkle').style.animation = 'sparkle 1.5s ease-in';
+    }, 2350);
+});
